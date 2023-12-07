c5fec3525630c0cdfb2742003980d520f9618e9f
crinulix <86973583+crinulix@users.noreply.github.com>
2023-12-07 11:49:52 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,14 @@
+-- Important requirements for Windows users
+-- NERDFonts a font that displays the necessary symbols
+-- External tools required
+-- Windows Terminal + pwsh
+-- mingw64 toolchain: https://www.msys2.org/
+-- ripgrep: https://github.com/BurntSushi/ripgrep -> necessary for Telescope 
+-- win32yank for clipboard integration
+-- sharkdp/fd
+-- nodejs for lsp 
+
+
 require("core.keymaps")
 require("core.plugins")
 require("core.plugin_config")
-
