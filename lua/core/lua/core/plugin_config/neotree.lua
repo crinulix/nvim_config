6c7e6483cd9e940c4ce3dff4638469aab8af1fc9
crinulix <86973583+crinulix@users.noreply.github.com>
2023-12-07 11:56:13 +0100

    Delete lua/core/lua/core directory

--- lua/core/lua/core/plugin_config/neotree.lua
+++
@@ -1,8 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require("neo-tree").setup()
-
--- key binding for the neotree toggle 
-vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
-
