1e64a85266833ba7aec8fd2876fe8767dc575004
crinulix <86973583+crinulix@users.noreply.github.com>
2023-12-08 15:55:16 +0100

    updated gruvbox to colorscheme

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -1,4 +1,4 @@
-require("core.plugin_config.gruvbox")
+require("core.plugin_config.colorscheme")
 require("core.plugin_config.lualine")
 require("core.plugin_config.neotree")
 require("core.plugin_config.telescope")
